8ecb46f4b708650daf5c868d8b3ae48f046e7fed
Terry Blue <terryblue@comcast.net>
2017-07-12 02:24:04 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,24 +1,33 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    binding.pry
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
   get '/figures/new' do
     erb :'/figures/new'
   end
 
   get '/figures/:id' do
+    binding.pry
     @figure = Figure.find(params[:id])
-    erb :'/figuress/show'
+    erb :'/figures/show'
   end
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
-    if params[:title_name].size > 0
-      @figure.title = Title.create(name: params[:title_name])
+    if params[:title].size > 0
+      @figure.titles << Title.create(params[:title])
     else
-      @figure.title = Title.find_by_id(params[:figure][:title_id])
+      binding.pry
+      @figure.titles << Title.find_by_id(params[:figure][:title_id])
     end
-    if params[:landmark_name].size > 0
-      @figure.landmark = Landmark.create(name: params[:landmark_name])
+    if params[:landmark].size > 0
+      @figure.landmarks << Landmark.create(params[:landmark])
     else
+      binding.pry
       @figure.landmark = Landmark.find_by_id(params[:figure][:landmark_id])
     end
     @figure.save
--- spec/controllers/figures_controller_spec.rb
+++ spec/controllers/figures_controller_spec.rb
@@ -29,7 +29,6 @@ describe FiguresController do
   it "allows you to create a new figure with a title" do
     visit '/figures/new'
     fill_in :figure_name, :with => "Doctor Who"
-    binding.pry
     check "title_#{Title.first.id}"
     click_button "Create New Figure"
     figure = Figure.last
